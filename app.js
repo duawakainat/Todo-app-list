56d9a27f8065885f962ba8f65f85a78438bf5648
duawakainat <kainathafeez04@gmail.com>
2023-10-30 05:57:24 -0700

    change

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 document.querySelector('#push').onclick = function () {
     const inputField = document.querySelector('.task_input');
     const inputValue = inputField.value;
@@ -5,6 +6,7 @@ document.querySelector('#push').onclick = function () {
     if (inputValue.length === 0) {
         alert('Please Enter a Value');
     } else {
+        // Create a new task element
         const newTask = document.createElement('div');
         newTask.classList.add('taskadd');
         newTask.innerHTML = `
@@ -12,31 +14,37 @@ document.querySelector('#push').onclick = function () {
         <button class='delete'><i class="fa-solid fa-trash" style="color: #3d84ff;"></i></button>
       `;
 
+        // Append the new task to the list
         document.querySelector('.taskS_input').appendChild(newTask);
 
+        // Clear the input field
         inputField.value = '';
 
+        // Add a click event listener to the delete button
         const deleteButton = newTask.querySelector('.delete');
         deleteButton.onclick = function () {
             newTask.remove();
             updateLocalStorage();
         }
 
+        // Update local storage
         updateLocalStorage();
     }
 }
 
+// Function to update local storage with current task list
 function updateLocalStorage() {
     const taskList = document.querySelectorAll('.taskadd span');
     const tasks = [];
 
     taskList.forEach(function (task) {
-        tasks.push(task);
+        tasks.push(task.textContent);
     });
 
     localStorage.setItem('todoList', JSON.stringify(tasks));
 }
 
+// Function to load tasks from local storage on page refresh
 function loadTasks() {
     const storedTasks = localStorage.getItem('todoList');
 
@@ -53,6 +61,7 @@ function loadTasks() {
 
             document.querySelector('.taskS_input').appendChild(newTask);
 
+            // Add a click event listener to the delete button
             const deleteButton = newTask.querySelector('.delete');
             deleteButton.onclick = function () {
                 newTask.remove();
@@ -62,4 +71,5 @@ function loadTasks() {
     }
 }
 
+// Load tasks when the page is refreshed
 window.addEventListener('load', loadTasks);
